4b6f1e433bca7feaeb5984f813c0947251dca192
Dmitriy Ribalko <ribalko@yandex.ru>
2019-04-18 00:31:07 +0300

    task 3

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,3 +1,6 @@
+const deadCounter = document.getElementById('dead');
+const lostCounter = document.getElementById('lost');
+
 function getHole( index ) {
     return document.getElementById('hole' + index);
 }
@@ -6,7 +9,12 @@ for (let i = 1; i < 9; i++) {
     elementHole = getHole( i );
     elementHole.addEventListener("click", function() {
       if ( elementHole.className.includes( 'hole_has-mole' ) ) {
-        console.log("впоймал!");
+        deadCounter.textContent = Number(deadCounter.textContent) + 1;
+        if (Number(deadCounter.textContent) == 10) alert("Победа!");
+      }
+      else {
+        lostCounter.textContent = Number(lostCounter.textContent) + 1;
+        if (Number(lostCounter.textContent) == 5) alert("Вы проиграли!");
       }
     });
 }
