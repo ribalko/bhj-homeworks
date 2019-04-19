a0c040c3823f2eed2aa68f4a163945af5dadb7c1
Dmitriy Ribalko <ribalko@yandex.ru>
2019-04-19 23:16:25 +0300

    task 1

---
+++ element-search/popups/task.js
@@ -0,0 +1,12 @@
+
+let modalMain = document.getElementById('modal_main');
+let modalMainClose = modalMain.getElementsByClassName('modal__close');
+let modalSuccess = document.getElementById('modal_success');
+let modalSuccessClose = modalSuccess.getElementsByClassName('modal__close');
+let showSuccess = document.getElementsByClassName('show-success');
+
+modalMain.classList.add("modal_active");
+
+showSuccess[0].addEventListener("click", function() { modalSuccess.classList.add("modal_active"); } );
+modalMainClose[0].addEventListener("click", function() { modalMain.classList.remove("modal_active"); } );
+modalSuccessClose[0].addEventListener("click", function() { modalSuccess.classList.remove("modal_active"); } );
