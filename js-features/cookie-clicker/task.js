37b7d77b4dc60592103ae637603eca64bd9948f0
Dmitriy Ribalko <ribalko@yandex.ru>
2019-04-25 00:16:00 +0300

    task 2

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -3,9 +3,7 @@ const cookieElement = document.getElementById('cookie');
 function cookieClick() {
     const cookieElement = document.getElementById('cookie');
     const counterElement = document.getElementById('clicker__counter');
-//  const speedElement = document.getElementById('clicker__speed');
     counterElement.textContent = Number(counterElement.textContent) + 1;
-//    curTime = new Date();
     if (cookieElement.width == 200) {    
         cookieElement.width = 220;
     }        
