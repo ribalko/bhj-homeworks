5b5e30ab0f4c5c9edce9595644eccd4a26ebcb2b
Dmitriy Ribalko <ribalko@yandex.ru>
2019-05-02 05:36:35 +0300

    syntax tabs

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -3,6 +3,21 @@ let valueElements = document.querySelectorAll('.dropdown__value');
 let ulElements = document.querySelectorAll('ul.dropdown__list');
 
 for(let i = 0; i < valueElements.length; i++){
-    valueElements[i].addEventListener("click",function() { if(ulElements[i].classList.contains('dropdown__list_active')) {ulElements[i].classList.remove('dropdown__list_active');} else {ulElements[i].classList.add('dropdown__list_active');} });
-    ulElements[i].addEventListener("click",function() {event.preventDefault(); valueElements[i].innerHTML = event.target.innerText; ulElements[i].classList.remove('dropdown__list_active');});    
+    valueElements[i].addEventListener("click",
+        function() {
+            if(ulElements[i].classList.contains('dropdown__list_active')) {
+                ulElements[i].classList.remove('dropdown__list_active');
+            }
+            else {
+                ulElements[i].classList.add('dropdown__list_active');
+            }
+        }
+    );
+    ulElements[i].addEventListener("click",
+        function() {
+            event.preventDefault();
+            valueElements[i].innerHTML = event.target.innerText;
+            ulElements[i].classList.remove('dropdown__list_active');
+        }
+    );    
 }
